8bd70263097e1700d3641e9b276310a2aa3a09ba
darkfusion90 <gauravsharmaa341@gmail.com>
2019-11-28 15:52:07 +0530

    Created stream components with initial boilerplate

---
+++ client/src/components/streams/StreamEdit.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const StreamEdit = () => {
+    return <div>Hello World!</div>;
+    
+    export default StreamEdit;
+}
---
+++ client/src/components/streams/StreamShow.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const StreamShow = () => {
+    return <div>Hello World!</div>;
+    
+    export default StreamShow;
+}
