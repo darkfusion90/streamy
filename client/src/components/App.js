f15e6d41ffe54cf9c660e367b68a02bc057c2049
darkfusion90 <gauravsharmaa341@gmail.com>
2019-11-28 15:55:52 +0530

    removed unnecessary react-dom import statement

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const PageOne = () => {
+    return (
+        <div>
+            Page One
+            <Link to="/two">Let's go to Page Two</Link>
+        </div>
+    );
+}
+
+const PageTwo = () => {
+    return (
+        <div>
+            Page Two
+            <Link to="/">Let's go to Page One</Link>
+        </div>
+    )
+}
 
 const App = () => {
-    return <div>Hello World!</div>;
+    return (
+        <div>
+            <BrowserRouter>
+                <Route path="/" component={PageOne} />
+                <Route path="/two" component={PageTwo} />
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 const StreamShow = () => {
     return <div>Hello World!</div>;
