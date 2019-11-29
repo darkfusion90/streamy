5208679d670e355dbe249154b1ac15c6610821af
darkfusion90 <gauravsharmaa341@gmail.com>
2019-11-29 12:49:48 +0530

    Added redux-thunk as middleware

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import { createStore, compose, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App'
 import reducers from './reducers'
@@ -10,7 +11,7 @@ import reducers from './reducers'
 //Wiring up redux devtools to allow viewing redux store in browser extension
 const composeEnhanchers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, composeEnhanchers(
-    applyMiddleware()
+    applyMiddleware(reduxThunk)
 ));
 
 ReactDOM.render(
