1feec4a2c41a8baaca57118d17175d4b4b243851
darkfusion90 <gauravsharmaa341@gmail.com>
2019-11-29 12:56:57 +0530

    Added streamsReducers to combineReducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 
-import {reducer as formReducer} from 'redux-form';
+import { reducer as formReducer } from 'redux-form';
 
-import changeAuthReducer from './changeAuthReducer'
+import changeAuthReducer from './changeAuthReducer';
+import { streamsReducer } from './streamsReducer';
 
 export default combineReducers({
     auth: changeAuthReducer,
-    form: formReducer
+    form: formReducer,
+    streams: streamsReducer
 });
