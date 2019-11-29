9ba0bd5ef4fb407d059192fbf237b930abf48edf
darkfusion90 <gauravsharmaa341@gmail.com>
2019-11-29 11:28:02 +0530

    Added Redux Form to Stream Create

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,7 +1,41 @@
 import React from 'react';
 
-const StreamCreate = () => {
-    return <div>StreamCreate</div>;
+import { Field, reduxForm } from 'redux-form';
+
+class StreamCreate extends React.Component {
+    renderTextInput(formProps) {
+        return (
+            <div className="field">
+                <label>{formProps.label}</label>
+                <input {...formProps.input} required/>
+            </div>
+        );
+    }
+
+    onFormSubmit(formValues) {
+        console.log(formValues);
+    }
+
+    render() {
+        return (
+            <form className="ui form" onSubmit={this.props.handleSubmit(this.onFormSubmit)}>
+                <Field
+                    name="title"
+                    component={this.renderTextInput}
+                    label="Enter Title" />
+                <Field
+                    name="description"
+                    component={this.renderTextInput}
+                    label="Enter Description" />
+
+                <button className="ui primary button">Create New Stream</button>
+            </form>
+        );
+    }
 }
 
-export default StreamCreate;
+export default reduxForm(
+    {
+        form: 'streamCreate'
+    }
+)(StreamCreate);
