9ced8e56ebde3e1512df6e2eb99cb3b68ee3deae
darkfusion90 <gauravsharmaa341@gmail.com>
2019-11-29 11:28:28 +0530

    Added redux form reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
 
+import {reducer as formReducer} from 'redux-form';
+
 import changeAuthReducer from './changeAuthReducer'
 
 export default combineReducers({
-    auth: changeAuthReducer
+    auth: changeAuthReducer,
+    form: formReducer
 });
