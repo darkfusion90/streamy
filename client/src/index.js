c6f364c1c2eaa1a42ade5b577641509b7c32641c
darkfusion90 <gauravsharmaa341@gmail.com>
2019-11-29 10:11:53 +0530

    Wired up react-devtools browser extension to work with store

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
 import App from './components/App'
 import reducers from './reducers'
 
-const store = createStore(reducers);
+//Wiring up redux devtools to allow viewing redux store in browser extension
+const composeEnhanchers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhanchers(
+    applyMiddleware()
+));
 
 ReactDOM.render(
     <Provider store={store}>
