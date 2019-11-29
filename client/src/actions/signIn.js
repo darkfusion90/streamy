3e4bc62372cb95688fef6f2f42637be45451de6c
darkfusion90 <gauravsharmaa341@gmail.com>
2019-11-29 10:12:08 +0530

    Added userId as payload

--- client/src/actions/signIn.js
+++ client/src/actions/signIn.js
@@ -1,7 +1,8 @@
-import {SIGN_IN} from './types';
+import { SIGN_IN } from './types';
 
-export const signIn = () => {
+export const signIn = (userId) => {
     return {
-        type: SIGN_IN
+        type: SIGN_IN,
+        payload: { userId: userId }
     }
 }
