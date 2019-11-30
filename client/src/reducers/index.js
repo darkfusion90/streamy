4ea93709f7256565b1346663f10be6f32ea97c4a
darkfusion90 <gauravsharmaa341@gmail.com>
2019-11-30 11:14:00 +0530

    Changed named import of streamsReducer to default import

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,7 +3,7 @@ import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 
 import changeAuthReducer from './changeAuthReducer';
-import { streamsReducer } from './streamsReducer';
+import streamsReducer from './streamsReducer';
 
 export default combineReducers({
     auth: changeAuthReducer,
