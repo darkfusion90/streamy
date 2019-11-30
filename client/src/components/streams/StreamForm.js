15c73764a6be9e5abe1a9beed52762c62d7c7afc
darkfusion90 <gauravsharmaa341@gmail.com>
2019-11-30 11:12:34 +0530

    Holds form functionality

---
+++ client/src/components/streams/StreamForm.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class StreamForm extends React.Component {
+    renderTextInput(formProps) {
+        return (
+            <div className="field">
+                <label>{formProps.label}</label>
+                <input {...formProps.input} required />
+            </div>
+        );
+    }
+
+    onFormSubmit = formValues => {
+        this.props.onFormSubmit(formValues);
+    }
+
+    render() {
+        return (
+            <form className="ui form" onSubmit={this.props.handleSubmit(this.onFormSubmit)}>
+                <Field
+                    name="title"
+                    component={this.renderTextInput}
+                    label="Enter Title" />
+                <Field
+                    name="description"
+                    component={this.renderTextInput}
+                    label="Enter Description" />
+
+                <button className="ui primary button">Create New Stream</button>
+            </form>
+        );
+    }
+}
+
+
+export default reduxForm(
+    {
+        form: 'streamForm'
+    }
+)(StreamForm);
