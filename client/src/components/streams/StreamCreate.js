2dd6d46309453e1e6c9fa7415ce46aaacc6bf545
darkfusion90 <gauravsharmaa341@gmail.com>
2019-11-30 11:11:27 +0530

    Migrated all Form functionality to StreamForm

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,41 +1,26 @@
 import React from 'react';
 
-import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+
+import { createStream } from '../../actions/stream-actions/index';
+import StreamForm from './StreamForm';
 
 class StreamCreate extends React.Component {
-    renderTextInput(formProps) {
-        return (
-            <div className="field">
-                <label>{formProps.label}</label>
-                <input {...formProps.input} required/>
-            </div>
-        );
-    }
-
-    onFormSubmit(formValues) {
-        console.log(formValues);
+    onFormSubmit = formValues => {
+        this.props.createStream(formValues);
     }
 
     render() {
         return (
-            <form className="ui form" onSubmit={this.props.handleSubmit(this.onFormSubmit)}>
-                <Field
-                    name="title"
-                    component={this.renderTextInput}
-                    label="Enter Title" />
-                <Field
-                    name="description"
-                    component={this.renderTextInput}
-                    label="Enter Description" />
-
-                <button className="ui primary button">Create New Stream</button>
-            </form>
+            <div>
+                <h3>Create new Stream</h3>
+                <StreamForm onFormSubmit={this.onFormSubmit} />
+            </div>
         );
     }
 }
 
-export default reduxForm(
-    {
-        form: 'streamCreate'
-    }
+export default connect(
+    null,
+    { createStream }
 )(StreamCreate);
