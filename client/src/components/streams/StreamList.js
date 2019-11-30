ea9b24bc84edaf7c3a318f6ecbf577765bc2121a
darkfusion90 <gauravsharmaa341@gmail.com>
2019-11-30 11:13:27 +0530

    Fetch and Display all sreams. Added administrative controls. Added "Create Stream" button

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,7 +1,90 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-const StreamList = () => {
-    return <div>StreamList</div>;
+import { fetchStreams } from '../../actions/stream-actions'
+
+class StreamList extends React.Component {
+    componentDidMount() {
+        this.props.fetchStreams();
+    }
+
+    renderAdministrativeControls(stream) {
+        if (stream.userId === this.props.currentUserId) {
+            return (
+                <div className="right floated content">
+                    <Link
+                        className="ui primary button"
+                        to={`/streams/edit/${stream.id}`}
+                    >
+                        Edit
+                    </Link>
+                    <Link
+                        className="ui negative button"
+                        to={`/streams/delete/${stream.id}`}
+                    >
+                        Delete
+                    </Link>
+                </div>
+            );
+        }
+    }
+
+    renderListOfStreams() {
+        //The admin controls will be rendered 'after' all content
+        //However, to make sure semantic-ui renders them properly,
+        //we place it before all content. The controls are 'floated right'
+        //essentially so they will appear at the end (i.e., right)
+        return this.props.streams.map(stream => {
+            return (
+                <div className="item" key={stream.id}>
+                    {this.renderAdministrativeControls(stream)}
+                    <i className="large middle aligned icon camera" />
+
+                    <div className="content">
+                        <h4 style={{ display: 'inline' }}>{stream.title}</h4>
+                        <div className="description">
+                            {stream.description}
+                        </div>
+                    </div>
+                </div>
+            )
+        });
+    }
+
+    renderCreateStreamButton() {
+        if (this.props.isSignedIn) {
+            return (
+                //push the button to right end of the screen
+                <div style={{ textAlign: "right" }}>
+                    <Link to="/streams/new" className="ui button primary">
+                        Create New Stream
+                    </Link>
+                </div>
+            );
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Streams:</h2>
+                <div className="ui celled list">{this.renderListOfStreams()}</div>
+                {this.renderCreateStreamButton()}
+            </div>
+        );
+    }
 }
 
-export default StreamList;
+const mapStateToProps = (state) => {
+    //Note that streams = {stream_id: stream, stream_id: stream, ...}
+    //We are only concerned about the stream (values) and not the stream_id (key) part
+    //So we just take the values part that is actually list of streams
+    return {
+        streams: Object.values(state.streams),
+        currentUserId: state.auth.userId,
+        isSignedIn: state.auth.isSignedIn
+    };
+}
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
