7459255e47e6cef5a66e155b97056fa53f15c0cd
darkfusion90 <gauravsharmaa341@gmail.com>
2019-11-30 15:19:37 +0530

    Created a new Modal

---
+++ client/src/components/Modal.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import history from '../history';
+
+class Modal extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isVisible: true,
+            isActive: true
+        }
+    }
+
+    hideModal = () => {
+        this.setState({
+            isVisible: false,
+            isActive: false
+        })
+    }
+
+    onClickApproveButton = () => {
+        this.hideModal();
+        this.props.onClickApproveButton();
+    }
+
+    onClickCancelButton = () => {
+        this.hideModal();
+        this.props.onClickCancelButton();
+    }
+
+    render() {
+        const visible = this.state.isVisible ? "visible" : "";
+        const active = this.state.isActive ? "active" : "";
+
+        return ReactDOM.createPortal(
+            <div onClick={()=>history.push('/')} className={`ui dimmer modals ${visible} ${active}`}>
+                <div
+                    onClick={e => e.stopPropagation()}
+                    className={`ui standard modal ${visible} ${active}`}
+                >
+                    <div className="header">{this.props.header}</div>
+                    <div className="content">
+                        <p>{this.props.content}</p>
+                    </div>
+                    <div className="actions">
+                        {this.props.actions}
+                    </div>
+                </div>
+            </div>,
+            document.querySelector("#modal")
+        )
+    }
+}
+
+export default Modal;
